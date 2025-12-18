a8c10c2dbb5b8c9a8c8b733963aa8fd50fee7f4b
ArfatChowdhury <138390501+ArfatChowdhury@users.noreply.github.com>
2025-12-18 23:59:45 +0600

    feat: Enhance homepage layout by adding sections for Top Products and New Arrivals, utilizing new components for improved product display.

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,18 +1,23 @@
 import getAllProducts from "../../../lib/getAllProducts"
+import NewArrivels from "./NewArrivels"
 import ProductCard from "./ProductCard"
+import TopProducts from "./TopProducts"
 
 export default async function HomePage() {
     const products = await getAllProducts()
-    // console.log(products, 'data')
     return (
         <div>
-            <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12 px-4 border-l-4 border-teal-500">
-                Latest Products
+           <div>
+           <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12 px-4 border-l-4 border-teal-500">
+                Top Products
             </h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4">
-                {products.map((product: any) => (
-                    <ProductCard key={product._id} product={product} />
-                ))}
+            <TopProducts products={products} />
+           </div>
+           <div>
+           <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12 px-4 border-l-4 border-teal-500">
+                New Arrivels
+            </h1>
+            <NewArrivels products={products} />
             </div>
         </div>
     )
