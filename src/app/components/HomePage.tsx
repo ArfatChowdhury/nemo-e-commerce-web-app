659b0146e93755a2c993674edcfaee850e10f1b2
ArfatChowdhury <138390501+ArfatChowdhury@users.noreply.github.com>
2025-12-19 00:12:31 +0600

    feat: Implement home page with hot deals, top products, and new arrivals sections including a "load more" feature.

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -7,18 +7,19 @@ export default async function HomePage() {
     const products = await getAllProducts()
     return (
         <div>
-           <div>
-           <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12 px-4 border-l-4 border-teal-500">
-                Top Products
-            </h1>
-            <TopProducts products={products} />
-           </div>
-           <div>
-           <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12 px-4 border-l-4 border-teal-500">
-                New Arrivels
-            </h1>
-            <NewArrivels products={products} />
+            <div>
+                <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12 px-4 border-l-4 border-teal-500">
+                    Top Products
+                </h1>
+                <TopProducts products={products} />
             </div>
+            <div>
+                <h1 className="text-3xl font-bold text-gray-800 mb-8 mt-12 px-4 border-l-4 border-teal-500">
+                    New Arrivels 🔥
+                </h1>
+                <NewArrivels products={products} />
+            </div>
+
         </div>
     )
 }
--- src/app/components/NewArrivels.tsx
+++ src/app/components/NewArrivels.tsx
@@ -1,28 +1,30 @@
+'use client';
+
 import React, { useState } from 'react'
 import ProductCard from './ProductCard';
 
-export default function NewArrivels() {
+export default function NewArrivels({ products }: { products: any }) {
     const [limit, setLimit] = useState(4);
 
     const handleLoadMore = () => {
         setLimit(prevLimit => prevLimit + 4);
     }
 
-    const displayedProducts = products.slice(0, limit);
+    const displayedProducts = [...products].reverse().slice(0, limit);
 
     return (
         <div className="div">
-            
+
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4">
 
-                {displayedProducts.reverse().map((product: any) => (
+                {displayedProducts.map((product: any) => (
                     <ProductCard key={product._id} product={product} />
                 ))}
             </div>
             {limit < products.length && (
                 <button className=" postion-absolute bottom-0  left-1000  bg-teal-600 text-white px-4 py-2 rounded-md hover:bg-teal-700 transition-colors duration-300" onClick={handleLoadMore}>Load More</button>
-             )}
-            
+            )}
+
 
 
         </div>
