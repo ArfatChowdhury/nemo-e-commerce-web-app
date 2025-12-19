54374ffdf1a4def55fca02e95cd68f98ec482591
ArfatChowdhury <138390501+ArfatChowdhury@users.noreply.github.com>
2025-12-20 00:13:50 +0600

    feat: Add WishlistButton component for managing product wishlists.

--- src/app/components/buttons/WishlistButton.tsx
+++ src/app/components/buttons/WishlistButton.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import React from 'react';
-import { FiHeart } from 'react-icons/fi';
-import { useAppDispatch } from '@/app/store/hooks';
-import { addToWishlist } from '@/app/store/slices/productFormSlice';
-
+import { useAppDispatch, useAppSelector } from '@/app/store/hooks';
+import { addToWishlist, removeFromWishlist } from '@/app/store/slices/productFormSlice';
+import { IoHeartOutline, IoHeart } from 'react-icons/io5';
 interface WishlistButtonProps {
     product: any;
     className?: string;
@@ -12,17 +11,25 @@ interface WishlistButtonProps {
 
 export default function WishlistButton({ product, className = "" }: WishlistButtonProps) {
     const dispatch = useAppDispatch();
+    const isProductInWishlist = useAppSelector((state) => state.productForm.wishlist.some((item: any) => item._id === product._id));
 
+    const toggleWishlist = () => {
+        if (isProductInWishlist) {
+            dispatch(removeFromWishlist(product));
+        } else {
+            dispatch(addToWishlist(product));
+        }
+    };
     return (
         <button
             onClick={(e) => {
                 e.preventDefault();
                 e.stopPropagation();
-                dispatch(addToWishlist(product));
+                toggleWishlist();
             }}
-            className={className || "absolute top-3 right-3 p-2 bg-white/80 backdrop-blur-sm rounded-full shadow-sm opacity-0 group-hover:opacity-100 transition-all duration-300 hover:bg-white hover:text-red-500 translate-y-2 group-hover:translate-y-0 text-gray-600 z-10"}
+            className={className || "absolute top-3 right-3 p-2 bg-white/80 backdrop-blur-sm rounded-full shadow-sm opacity-0 group-hover:opacity-100 transition-all duration-300 hover:bg-white hover:text-red-500 translate-y-2 group-hover:translate-y-0 text-gray-600 z-10" + (isProductInWishlist ? " text-red-500" : "")}
         >
-            <FiHeart size={18} />
+            {isProductInWishlist ? <IoHeart size={18} className="text-red-500" /> : <IoHeartOutline size={18} />}
         </button>
     );
 }
